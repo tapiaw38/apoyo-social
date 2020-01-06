4950dd49bf0bc64c668ccd1556f45f637009d0fe
Walter <50256112+tapiaw38@users.noreply.github.com>
2020-01-06 00:51:59 -0300

    Correccion error monto

--- ventana_sub.py
+++ ventana_sub.py
@@ -29,8 +29,14 @@ def ven_sub():
         elif len(borra2.get())>8 or len(borra2.get())<8:
             messagebox.showerror("ERROR", "Debes ingresar un DNI sin puntos \nDebe tener 8 caracteres")
             ventana.deiconify()
+        elif not borra2.get().isdigit():
+            messagebox.showerror("ERROR", "Debes inresar un DNI correcto")
+            ventana.deiconify()
         elif borra3.get() == "":
             messagebox.showerror("ERROR", "Debes completar todos lo campos")
+            ventana.deiconify()
+        elif not borra3.get().isdigit():
+            messagebox.showerror("ERROR", "Debes inresar un monto correcto")
             ventana.deiconify()
         elif borra4.get() == "":
             messagebox.showerror("ERROR", "Debes completar todos lo campos")
@@ -38,11 +44,20 @@ def ven_sub():
         elif len(data1.get())>2 or len(data1.get())<2:
             messagebox.showerror("ERROR", "El formato de fecha es incorrecto \nEjemplo: 01-01-2019")
             ventana.deiconify()
+        elif not data1.get().isdigit():
+            messagebox.showerror("ERROR", "Dia incorrecto")
+            ventana.deiconify()
         elif len(data2.get())>2 or len(data2.get())<2:
             messagebox.showerror("ERROR", "El formato de fecha es incorrecto \nEjemplo: 01-01-2019")
             ventana.deiconify()
+        elif not data2.get().isdigit():
+            messagebox.showerror("ERROR", "Mes incorrecto")
+            ventana.deiconify()
         elif len(data3.get())>4 or len(data3.get())<4:
             messagebox.showerror("ERROR", "El formato de fecha es incorrecto \nEjemplo: 01-01-2019")
+            ventana.deiconify()
+        elif not data3.get().isdigit():
+            messagebox.showerror("ERROR", "Año incorrecto")
             ventana.deiconify()
         else:
             miCursor.execute("INSERT INTO subsidios VALUES(NULL, '" + borra1.get() +
